917a0bfd70b3bd9ddd46046577a1a4588da9da02
jeong <jeong.junhueck@gmail.com>
2024-08-02 23:25:08 +0900

    WeakSetAndMap: garbageCollection

--- array-practice/weakSetweakMap.js
+++ array-practice/weakSetweakMap.js
@@ -1,6 +1,16 @@
 let person = {name: 'Max'};
-// only object
+// only object, not numbers or string
 const persons = new WeakSet();
 persons.add(person);
 
+// make garbage collection process
+person = null;
 console.log(persons);
+
+
+let person2 = {name: 'Min'};
+const personData = new WeakMap();
+personData.set(person2, 'Extra info');
+person = null;
+console.log(personData);
+
