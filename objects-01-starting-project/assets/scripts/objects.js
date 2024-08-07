df79ed04aa5b091b416a28125561f48ce423ad62
jeong <jeong.junhueck@gmail.com>
2024-08-07 21:33:50 +0900

    Object-oriented programming

--- objects-01-starting-project/assets/scripts/objects.js
+++ objects-01-starting-project/assets/scripts/objects.js
@@ -42,7 +42,7 @@ const renderMovies = (filter = '') => {
     getFormattedTitle = getFormattedTitle.bind(movie);
     let text = getFormattedTitle() + ' - ';
     for(const key in info) {
-      if(key !== 'title') {
+      if(key !== 'title' && key !== '_title') {
         text = text + `${key}` + `${info[key]}`;
       }
     }
@@ -56,13 +56,25 @@ const addMovieHandler = () => {
   const extraName = document.getElementById('extra-name').value;
   const extraValue = document.getElementById('extra-value').value;
 
-  if(title.trim() === '' || extraName.trim() === '' || extraValue.trim() === ''){
+  if(
+    extraName.trim() === '' ||
+    extraValue.trim() === ''){
     return;
   }
 
   const newMovie = {
     info: {
-      title,
+      // each property can have: getter and setter
+      set title(val) {
+        if(val.trim() === ''){
+          this._title = 'DEFAULT';
+          return;
+        }
+        this._title = val;
+      },
+      get title() {
+        return this._title;
+      },
       [extraName]: extraValue
     },
   id: Math.random().toString(),
@@ -72,6 +84,9 @@ const addMovieHandler = () => {
     return this.info.title.toUpperCase();
   }
   };
+
+  newMovie.info.title = title;
+  console.log(newMovie.info.title);
 
   movies.push(newMovie);
   renderMovies();
