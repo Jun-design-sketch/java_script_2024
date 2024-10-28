3a467958b59cb566dd6c04894bfafa05a2b2a577
jeong <jeong.junhueck@gmail.com>
2024-10-28 23:49:46 +0900

    add publicPath

--- tooling-02-initialized-npm/webpack.config.js
+++ tooling-02-initialized-npm/webpack.config.js
@@ -1,14 +1,21 @@
 /* eslint-disable no-undef */
 const path = require('path');
-
+// npm run build
 // NodeJSに右側のオブジェクトを外にはみ出せる
 // eslint-disable-next-line no-undef
 module.exports = {
   // entryPointを指定すれば、そこから全ての依存性を確認する
   // inputフォルダ（bundling前）とoutputフォルダを分けないといけない
+  // 複数のページをbundle化したいのであればこうなる。。
+  // entry : {
+  // welcome: './src/welcome/welcome.js'
+  // about: './src/about-page/about.js'
+  // }
+  mode: 'development',
   entry: './src/app.js',
   output: {
     filename: 'app.js',
-    path: path.resolve(__dirname, 'assets', 'scripts')
+    path: path.resolve(__dirname, 'assets', 'scripts'),
+    publicPath: 'assets/scripts/'
   }
 };
