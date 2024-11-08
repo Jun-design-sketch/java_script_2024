17a8656ec5db0a54ccbdc26a86ef5b013e2732f6
jeong <jeong.junhueck@gmail.com>
2024-11-09 00:39:51 +0900

    show, hide

--- practice-places-01-starting-setup/src/UI/Modal.js
+++ practice-places-01-starting-setup/src/UI/Modal.js
@@ -2,27 +2,47 @@ export class Modal {
   constructor(contentId, fallbackText) {
     this.fallbackText = fallbackText;
     this.contentTemplateEl = document.getElementById(contentId);
-    this.modalTemplateEl = document.getElementById('modal-template');
+    this.modalTemplateEl = document.getElementById("modal-template");
   }
 
   show() {
-    if ('content' in document.createElement('template')) {
-      const modalElements = document.importNode(this.modalTemplateEl.content, true);
-      const modalElement = modalElements.querySelector('.modal');
-      const backdropElement = modalElements.querySelector('.backdrop');
+    if ("content" in document.createElement("template")) {
+      // 再帰的なNodeまたはDocumentFragmentのコピーを作る。
+      this.modalElements = document.importNode(
+        this.modalTemplateEl.content,
+        true
+      );
+      // 各要素を確保する
+      this.modalElement = this.modalElements.querySelector(".modal");
+      this.backdropElement = this.modalElements.querySelector(".backdrop");
+      // contentIdのcontentを取得する
       const contentElement = document.importNode(
         this.contentTemplateEl.content,
         true
       );
-
-      modalElement.appendChild(contentElement);
-
-      document.body.insertAdjacentElement('afterbegin', modalElement);
-      document.body.insertAdjacentElement('afterbegin', backdropElement);
+      // つける
+      this.modalElement.appendChild(contentElement);
+      // document.bodyに挿入する
+      document.body.insertAdjacentElement("afterbegin", this.modalElement);
+      document.body.insertAdjacentElement("afterbegin", this.backdropElement);
     } else {
       alert(this.fallbackText);
     }
   }
 
-  hide() {}
+  hide() {
+    // 単純にdisplay='none'にしてみた
+    // classNameで探すと配列が返される
+    // const bdp = document.getElementsByClassName("backdrop");
+    // const mdl = document.getElementsByClassName("modal");
+    // for(let i=0; i<bdp.length; i++) bdp[i].style.display = 'none';
+    // for(let i=0; i<mdl.length; i++) mdl[i].style.display = 'none';
+
+    if(this.modalElement) document.body.removeChild(this.modalElement);
+    if(this.backdropElement) document.body.removeChild(this.backdropElement);
+    // もしくは this.modalElement.remove();
+
+    this.modalElement = null;
+    this.backdropElement = null;
+  }
 }
